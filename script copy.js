6df895fb6ee76b094aaed8ca5ed4319e44528c12
Sunveg <54052697+Sunveg@users.noreply.github.com>
2020-06-16 01:26:59 +0530

    Add files via upload

--- script copy.js
+++ script copy.js
@@ -1,5 +1,16 @@
 var i = 0;
 
+function fun() {
+    document.getElementById('player1_img').style.visibility="hidden";
+    document.getElementById('player2_img').style.visibility="hidden";
+}
+fun();
+
+function fun2() {
+    document.getElementById('player1_img').style.visibility="visible";
+    document.getElementById('player2_img').style.visibility="visible";
+}
+
 function getData(name) {
     fetch('PL.json').then(function(response) {
         return response.json();
@@ -9,12 +20,25 @@ function getData(name) {
         for( i = 0; i<data.length;i++) {
             if(data[i].Player == name)
             {
+                var firstname = name.split(' ');
+                var first = firstname[0];
+                if(firstname.length>2) {
+                    var last = firstname[firstname.length-1];
+                }
+                else{
+                    var last = firstname[1];
+                }
                 document.getElementById('name').textContent = name;
                 document.getElementById('team').textContent = data[i].Squad;
                 document.getElementById('nationality').textContent = data[i].Nation;
                 document.getElementById('goals').textContent = data[i].Gls;
                 document.getElementById('assists').textContent = data[i].Ast;
                 document.getElementById('app').textContent = data[i].MP;
+                document.getElementById('age').textContent = data[i].Age;
+                document.getElementById('pos').textContent = data[i].Pos;
+                document.getElementById('starts').textContent = data[i].Starts;
+                document.getElementById('mp').textContent = data[i].Min;
+                img(first, data[i].Squad, name, last);
             }
         }
 
@@ -35,12 +59,27 @@ function getData2(name) {
         for( i = 0; i<data.length;i++) {
             if(data[i].Player == name)
             {
+                var firstname = name.split(' ');
+                var first = firstname[0];
+                if(firstname.length>2) {
+                    var last = firstname[firstname.length-1];
+                }
+                else{
+                    var last = firstname[1];
+                }
+                var teamname = data[i].Squad.split(' ');
+                teamname_first = teamname[0];
                 document.getElementById('name2').textContent = name;
                 document.getElementById('team2').textContent = data[i].Squad;
                 document.getElementById('nationality2').textContent = data[i].Nation;
                 document.getElementById('goals2').textContent = data[i].Gls;
                 document.getElementById('assists2').textContent = data[i].Ast;
                 document.getElementById('app2').textContent = data[i].MP;
+                document.getElementById('age2').textContent = data[i].Age;
+                document.getElementById('pos2').textContent = data[i].Pos;
+                document.getElementById('starts2').textContent = data[i].Starts;
+                document.getElementById('mp2').textContent = data[i].Min;
+                img2(first, data[i].Squad, name, last, teamname_first);
             }
         }
 
@@ -54,6 +93,7 @@ function getData2(name) {
 
 function test()
         {
+            fun2();
             var userInput = document.getElementById("myInput").value;
             getData(userInput);
             var userInput2 = document.getElementById("myInput2").value;
@@ -61,6 +101,10 @@ function test()
             document.getElementById("GOALS").textContent = 'GOALS';
             document.getElementById("ASSISTS").textContent = 'ASSISTS';
             document.getElementById("APPEARANCES").textContent = 'APPEARANCES';
+            document.getElementById("AGE").textContent = 'AGE';
+            document.getElementById("POSITION").textContent = 'POSITION';
+            document.getElementById("STARTS").textContent = 'STARTS';
+            document.getElementById("MP").textContent = 'MINUTES';
         }
 
 
@@ -84,12 +128,48 @@ function filter_names() {
     })
 }
 
-function selected_player(element) {
-    player = element;
-    console.log(player);
+function img(name, team, fullname, lastname) {
+    var y = 0;
+    fetch('https://raw.githubusercontent.com/hendrasan/epl1920/master/epl1920stat.json').then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        console.log(data);
+        for( i = 0; i<data.length;i++) {
+            if((data[i].firstName == name && data[i].team == team) || data[i].name == fullname || data[i].lastName == lastname)
+            {
+                document.getElementById('player1_img').src = data[i].picture;
+                y = 1;
+            }
+        }
+        if(y == 0)
+        {
+            document.getElementById('player1_img').title = "Sorry, player photo not available";
+            document.getElementById('player1_img').src = "unavailable.png";
+        }
+    })
+}
+
+function img2(name2, team2, fullname, lastname) {
+    var y = 0;
+    fetch('https://raw.githubusercontent.com/hendrasan/epl1920/master/epl1920stat.json').then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        console.log(data);
+        for( i = 0; i<data.length;i++) {
+            if((data[i].firstName == name2 && data[i].team == team2) || data[i].name == fullname || data[i].lastName == lastname)
+            {
+                document.getElementById('player2_img').src = data[i].picture;
+                y = 1;
+            }
+        }
+        if(y == 0)
+        {
+            document.getElementById('player2_img').title = "Sorry, player photo not available";
+            document.getElementById('player2_img').src = "unavailable.png";
+        }
+    })
 }
 
 filter_names();
-
 
 //getData("Sergio Aguero")
